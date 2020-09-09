622898c43aed460e01e80f3a3917668bd2754467
Paul van Buuren <paul@wbvb.nl>
2020-09-09 13:55:07 +0200

    code formatting applied

--- waymark-initiatieven-led.php
+++ waymark-initiatieven-led.php
@@ -1,5 +1,5 @@
 <?php
-	
+
 /*
 Plugin Name: Waymark Digitale Overheid
 Plugin URI: https://www.joesway.ca/waymark/demo/
@@ -22,24 +22,24 @@ add_action( 'plugins_loaded', 'waymark_load_plugin_textdomain' );
 
 
 //Base
-require_once('inc/Waymark_Config.php');
-require_once('inc/Waymark_Types.php');
-require_once('inc/Waymark_Taxonomies.php');
-require_once('inc/Waymark_Install.php');
+require_once( 'inc/Waymark_Config.php' );
+require_once( 'inc/Waymark_Types.php' );
+require_once( 'inc/Waymark_Taxonomies.php' );
+require_once( 'inc/Waymark_Install.php' );
 
 //Objects
-require_once('inc/Objects/Waymark_Map.php');
-require_once('inc/Objects/Waymark_Collection.php');
+require_once( 'inc/Objects/Waymark_Map.php' );
+require_once( 'inc/Objects/Waymark_Collection.php' );
 
 //Helpers
-require_once('inc/Helpers/Waymark_Helper.php');
-require_once('inc/Helpers/Waymark_Input.php');
+require_once( 'inc/Helpers/Waymark_Helper.php' );
+require_once( 'inc/Helpers/Waymark_Input.php' );
 
 //Front
-require_once('inc/Waymark_Front.php');
+require_once( 'inc/Waymark_Front.php' );
 
 //Admin
-require_once('inc/Waymark_Admin.php');
+require_once( 'inc/Waymark_Admin.php' );
 
 
 //========================================================================================================
@@ -49,10 +49,8 @@ require_once('inc/Waymark_Admin.php');
  */
 
 
-
- 
 function waymark_load_plugin_textdomain() {
-	
+
 	load_plugin_textdomain( "waymark", false, basename( dirname( __FILE__ ) ) . '/languages/' );
 
 }
@@ -62,15 +60,15 @@ function waymark_load_plugin_textdomain() {
 // Google Maps API key
 
 function my_acf_init() {
-    acf_update_setting('google_api_key', 'AIzaSyB5vVMUunag3KLrIcsMK9dWhVhHVzj_Ub0');
+	acf_update_setting( 'google_api_key', 'AIzaSyB5vVMUunag3KLrIcsMK9dWhVhHVzj_Ub0' );
 }
 
-add_action('acf/init', 'my_acf_init');
+add_action( 'acf/init', 'my_acf_init' );
 
 //========================================================================================================
 
 // Add the ACF (advanced custom fields) 
-
+/*
 if( function_exists('acf_add_local_field_group') ):
 
 acf_add_local_field_group(array(
@@ -116,6 +114,5 @@ acf_add_local_field_group(array(
 ));
 
 endif;
-
+*/
 //========================================================================================================
-
